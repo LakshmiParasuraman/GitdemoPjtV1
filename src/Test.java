538801665a6ae78bc29091b961dfef8b5397ff44
lakshmiparasuraman <lakshmiparasuraman@localhost>
2022-10-13 12:36:54 -0400

    First Commit

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+
+	
+	public static void main(String[] args)
+	{
+		
+		String hungry = "yes";
+		
+		if(hungry == "Yes")
+		{
+			System.out.println("Eat Something");
+			
+		}
+		else
+		{
+			System.out.println("Don't eat");
+		}
+	}
+}
